bd7d6504c66b4cf3ff48f7de8e56c6faf96ddf8c
Josue Alvarez <josuetec2003@gmail.com>
2017-06-21 13:13:36 -0600

    Modificaciones en server y se agrego jquery

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const PORT = 11003
 app.set('view engine', 'pug')
 app.use(express.static(__dirname + '/static'))
 
-app.get('/', (req, res) => {	
-	res.render('index.pug')
+app.get('/', (req, res) => {
+	var langs = ['C#', 'C++', 'Javascript', 'VB.Net', 'Python', 'PHP', 'HTML5/CSS3']	
+	res.render('index.pug', {nombre: 'Josue', edad: 31, langs: langs})
 })
 
 app.listen(PORT, (err) => {
